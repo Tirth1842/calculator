d29b4098172bf120dee887d352fcb6c78a9fc53a
Tirth Patel <tirthpatel.1842000@gmail.com>
2021-08-04 12:53:07 +0530

    creating the component

---
+++ src/Components/Button.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Button extends Component {
+    render (){
+        return (
+            <div>welcome to button</div>
+        );
+    }
+}
+
+export default Button;
---
+++ src/Components/ButtonPanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ButtonPanel extends Component {
+    render () {
+        return (
+            <div>welcome to the button pannel</div>
+        );
+    }
+}
+
+export default ButtonPanel;
